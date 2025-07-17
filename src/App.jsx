09d3c40ea95453acbb420318c061730b8c97b8e1
Maia.dev <asmodeoanxy@gmail.com>
2025-07-17 20:17:46 -0300

    plantilla terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+import ChatScreen from './Screens/ChatScreen/ChatScreen'
+import { Route, Routes } from 'react-router'
+import ContactScreen from './Screens/ContactScreen/ContactScreen'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div>
+			<Routes>
+				<Route
+					path='/'
+					element={<ContactScreen/>}
+				/>
+				{/*
+				:contact_id es un parametro de busqueda (valor pasado por la URL)
+				ponemos :contact_id para indicar que el valor es variable, es decir puede ser:
+				/contact/1/messages
+				o 
+				/contact/2/messages
+				etc...
+				*/}
+				<Route 
+					path='/contact/:contact_id/messages' 
+					element={<ChatScreen/>} 
+				/>
+
+			</Routes>
+
+		</div>
+	)
 }
 
 export default App
