c30f5a3a42399d41f76bfe62bf2bdb7750e559f6
Maia.dev <asmodeoanxy@gmail.com>
2025-07-25 10:56:44 -0300

    algo

--- src/Screens/ContactScreen/ContactScreen.jsx
+++ src/Screens/ContactScreen/ContactScreen.jsx
@@ -8,20 +8,15 @@ const ContactScreen = () => {
     const [contactsState, setContactsState] = useState(contacts);
 
     return (
-        <div >
+        <div className='contact-screen'>
             <ContactList contacts={contactsState} />
-				<nav>
-					<li><NavLink to="/contact/1/messages">Contacto 1</NavLink> </li>
-					<li><NavLink to="/contact/2/messages">Contacto 2</NavLink> </li>
-					<li><NavLink to="/contact/3/messages">Contacto 3</NavLink> </li>
-				</nav>
+		{/* 		<nav>
+					<li><NavLink to="/contact/1/messages"></NavLink> </li>
+					<li><NavLink to="/contact/2/messages"></NavLink> </li>
+					<li><NavLink to="/contact/3/messages"></NavLink> </li>
+				</nav> */}
     </div>
     );
 };
 
 export default ContactScreen;
-
-/* 
-v1:
-Hace un componente de React llamado ContactScreen, que tenga un estado con la lista de contactos, dicha lista debera renderizarse dentro del componente, por cada contacto quiero que crees un <div></div> con la informacion del contacto. Observa las propiedades de cada contacto para mostrarlas en la pantalla, necesito que dividas el renderizado de la lista en un componente aparte llamado ContactList que reciba el estado de la lista y cada ContactItem tambien sera un componente aparte. 
-*/
---
+++ src/assets/Icons.jsx
@@ -0,0 +1,12 @@
+import { BsBullseye, BsPeople  } from "react-icons/bs";
+import { MdOutlineVideocam } from "react-icons/md";
+import { PiChatCircleDotsBold } from "react-icons/pi";
+
+const ICONS = {
+    video: MdOutlineVideocam,
+    state: BsBullseye,
+    channels: PiChatCircleDotsBold,
+    community: BsPeople,
+}
+
+export default ICONS
